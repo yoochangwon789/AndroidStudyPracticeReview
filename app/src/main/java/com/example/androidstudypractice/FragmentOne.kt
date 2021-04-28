dcd181bf908b9ab17023ec491db8761068ca3616
changwon <ckddnjs554@naver.com>
2021-04-28 20:44:28 +0900

    Feat : Fragment 사용방법 정의와 LifeCycle Log 출력 Review

--- app/src/main/java/com/example/androidstudypractice/FragmentOne.kt
+++ app/src/main/java/com/example/androidstudypractice/FragmentOne.kt
@@ -2,18 +2,22 @@ package com.example.androidstudypractice
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentOne: Fragment() {
 
     override fun onAttach(context: Context) {
+        Log.d("F_life_cycle", "F_onAttach")
         super.onAttach(context)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("F_life_cycle", "F_onCreate")
         super.onCreate(savedInstanceState)
     }
 
@@ -22,38 +26,48 @@ class FragmentOne: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        Log.d("F_life_cycle", "F_onCreateView")
+
+        return inflater.inflate(R.layout.fargment_one, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("F_life_cycle", "F_onViewCreated")
         super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d("F_life_cycle", "F_onActivityCreated")
         super.onActivityCreated(savedInstanceState)
     }
 
     override fun onStart() {
+        Log.d("F_life_cycle", "F_onStart")
         super.onStart()
     }
 
     override fun onResume() {
+        Log.d("F_life_cycle", "F_onResume")
         super.onResume()
     }
 
     override fun onPause() {
+        Log.d("F_life_cycle", "F_onPause")
         super.onPause()
     }
 
     override fun onStop() {
+        Log.d("F_life_cycle", "F_onStop")
         super.onStop()
     }
 
     override fun onDestroyView() {
+        Log.d("F_life_cycle", "F_onDestroyView")
         super.onDestroyView()
     }
 
     override fun onDetach() {
+        Log.d("F_life_cycle", "F_onDetach")
         super.onDetach()
     }
 }
