79c419a921b0e25e8671aa92c198621d85d10921
changwon <ckddnjs554@naver.com>
2021-04-28 21:05:06 +0900

    Review : Fragment 동적으로 사용하는 방법

--- app/src/main/java/com/example/androidstudypractice/FragmentActivity.kt
+++ app/src/main/java/com/example/androidstudypractice/FragmentActivity.kt
@@ -3,12 +3,23 @@ package com.example.androidstudypractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
         Log.d("life_cycle", "onCreate")
+
+        button.setOnClickListener {
+            val fragmentOne: FragmentOne = FragmentOne()
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container_view, fragmentOne)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onStart() {
